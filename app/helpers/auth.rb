7fb8ffe4ed0642a21ed2fa637eb3de8e7e6765f3
Vitaliy Tokmyanin <shamik2024@gmail.com>
2021-04-14 21:47:50 +0300

    rm_comment

--- app/helpers/auth.rb
+++ app/helpers/auth.rb
@@ -1,5 +1,4 @@
 module Auth
-  # AUTH_TOKEN = %r{\ABearer (?<token>.+)\z}
   extend self
 
   def extracted_token(token)
@@ -7,17 +6,4 @@ module Auth
   rescue JWT::DecodeError
     {}
   end
-
-  # private
-
-  # def matched_token
-  #   result = auth_header&.match(AUTH_TOKEN)
-  #   return if result.blank?
-
-  #   result[:token]
-  # end
-
-  # def auth_header
-  #   request.env['HTTP_AUTHORIZATION']
-  # end
 end
